0a9e025d223d390819135653fe8f4c07baf4491e
ahmedmsaaid <ahmedmsaaid908@gmail.com>
2024-09-25 21:37:30 +0300

    add Localization , auth with google and username and password , add fireStore

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:dart_openai/dart_openai.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:voicify/data/core/api/end_points.dart';
 import 'package:voicify/my_app.dart';
 import 'data/cache/cache_helper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'data/keys/keys.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  OpenAI.apiKey =
-      "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";
+  OpenAI.apiKey = Keys.openAi;
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-
   await SharedHelper.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
