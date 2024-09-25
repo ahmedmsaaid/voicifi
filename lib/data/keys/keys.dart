56074ed6641b50132e4caa13fa85ea8b071b32ac
Elprof <66503823+ahmedmsaaid@users.noreply.github.com>
2024-09-25 21:44:51 +0300

    Update keys.dart

--- lib/data/keys/keys.dart
+++ lib/data/keys/keys.dart
@@ -1,4 +1,1 @@
-class Keys {
-  static final String openAi =
-      "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";
-}
+
